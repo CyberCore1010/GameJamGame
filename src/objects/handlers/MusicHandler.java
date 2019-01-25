d631977ec873f17cad01c1289fb6a0471e8ea069
Jakub <NSSteam7@gmail.com>
2019-01-25 20:58:51 +0000

    L&R variants added

--- src/objects/handlers/MusicHandler.java
+++ src/objects/handlers/MusicHandler.java
@@ -8,74 +8,101 @@ import net.beadsproject.beads.ugens.*;
 import java.util.HashMap;
 import java.util.Scanner;
 
-public class MusicPlayer {
+public class MusicHandler {
     private AudioContext ac;
     public Glide fade;
     private Gain playingTrack;
-    private HashMap<String,Sample> trackList;
+    public HashMap<String,Sample> trackList;
 
-    public MusicPlayer(int transitionSpeed){
+    public MusicHandler(){
         this.ac = new AudioContext();
         trackList = new HashMap<>();
-        fade = new Glide(ac,1,transitionSpeed);
+//        fade = new Glide(ac,1,transitionSpeed);
+        trackList.put("alarm",SampleManager.sample("res/audio/alarm.mp3"));
+//        trackList.put("doom1",SampleManager.sample("res/audio/doom1.mp3"));
+//        trackList.put("doom2",SampleManager.sample("res/audio/doom2.mp3"));
         trackList.put("night",SampleManager.sample("res/audio/night.mp3"));
+        trackList.put("whisper1",SampleManager.sample("res/audio/whisper1.mp3"));
+        trackList.put("whisper1Right",SampleManager.sample("res/audio/whisper1Right.mp3"));
+        trackList.put("whisper1Left",SampleManager.sample("res/audio/whisper1Left.mp3"));
+        trackList.put("whisper2",SampleManager.sample("res/audio/whisper2.mp3"));
+        trackList.put("woosh",SampleManager.sample("res/audio/woosh.mp3"));
     }
 
 
-    public void playTrack(String track){
-//		SampleManager.setBufferingRegime(Sample.Regime.newStreamingRegime(1000));
-        SamplePlayer player = new SamplePlayer(ac, trackList.get(track));
+//    public void playTrack(String track){
+//        SamplePlayer player = new SamplePlayer(ac, trackList.get(track));
 //        fade.setGlideTime(5000);
 //        fade.setValue(1);
-        playingTrack = new Gain(ac,5,fade);
-        playingTrack.addInput(player);
-        ac.out.addInput(playingTrack);
-    }
+//        playingTrack = new Gain(ac,5,fade);
+//        player.setLoopType(SamplePlayer.LoopType.LOOP_FORWARDS);
+//        playingTrack.addInput(player);
+//        ac.out.addInput(playingTrack);
+//    }
 
-    public void changeTrack(String track){
-//        fade.setGlideTime(5000);
-//        fade.setValue(0);
-        playingTrack.kill();
-        playTrack(track);
+    public AudioContext getAC(){
+        return(ac);
     }
-
-    public void changeVolume(float speed){
-        fade.setValue(speed);
-    }
-
 
     public void start(){
         ac.start();
     }
+
     public void stop(){
         ac.stop();
     }
 
+//    public void changeTrack(String track){
+//        fade.setGlideTime(5000);
+//        fade.setValue(0);
+//        playingTrack.kill();
+//        playTrack(track);
+//    }
+
     //TEST function to check if audio playing and libs work
     public static void main(String[] args) {
-
-        MusicPlayer music = new MusicPlayer(2000);
-        music.playTrack("night");
+        MusicHandler music = new MusicHandler();
         music.start();
+        MusicPlayer track2 = new MusicPlayer(music.getAC(),music.trackList.get("night"),2,2000);
+        MusicPlayer track1 = new MusicPlayer(music.getAC(),music.trackList.get("whisper1"),1,2000);
         Scanner sc = new Scanner(System.in);
         while(true){
             float i = sc.nextFloat();
-            music.fade.setValue(i);
-//            music.changeTrack(i);
+            int n = sc.nextInt();
+            track1.fade(i,n);
         }
 
+    }
+}
 
-//        AudioContext ac;
-//
-//        ac = new AudioContext();
-//
-//        Noise n = new Noise(ac);
-//
-//        Gain g = new Gain(ac, 1, 0.1f);
-//
-//        g.addInput(n);
-//        ac.out.addInput(g);
-//
-//        ac.start();
+class MusicPlayer{
+    private AudioContext ac;
+    private Sample track;
+    private SamplePlayer player;
+    private Glide fade;
+    private Gain g;
+
+    public MusicPlayer(AudioContext ac, Sample track, float volume, int transitionSpeed){
+        this.track = track;
+        this.ac = ac;
+        this.player = new SamplePlayer(ac,track);
+        this.fade = new Glide(ac,volume,transitionSpeed);
+        player.setLoopType(SamplePlayer.LoopType.LOOP_FORWARDS); //default loop forwards
+        g = new Gain(ac,5,fade);
+        g.addInput(player);
+        ac.out.addInput(g);
+    }
+    public void fade(float targetVol,int transition){
+        fade.setValue(targetVol);
+        fade.setGlideTime(transition);
+    }
+    public void start(){
+        player.start();
+    }
+    public void stop(){
+        player.pause(true);
+    }
+    public void kill(){
+        player.kill();
     }
 }
