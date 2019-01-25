f1d7916ac827c1b4659576c22f94e95ea1f2d699
CyberCore1010 <39372030+CyberCore1010@users.noreply.github.com>
2019-01-25 15:56:13 +0000

    FileIO

---
+++ src/objects/FileIO/LevelLoader.java
@@ -0,0 +1,19 @@
+package objects.FileIO;
+
+import init.Game;
+
+public class LevelLoader {
+    ObjectFileReader mapContent;
+    Game game;
+
+    LevelLoader(Game game) {
+        this.game = game;
+    }
+
+    public void LoadLevel(String fileName) {
+        mapContent = new ObjectFileReader(fileName, game);
+        while(mapContent.hasNextObject()) {
+            game.objectHandler.objects.add(mapContent.getNextObject());
+        }
+    }
+}
