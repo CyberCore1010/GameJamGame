9df7146cbbeee5f236847565e3992386b2b2d592
CyberCore1010 <39372030+CyberCore1010@users.noreply.github.com>
2019-01-26 10:57:53 +0000

    Map making with images
    -Added a method to make a map (Which is a two dimensional array of ints, 1 for a place the AI can go, and 0 for a place it can't)
    -Map will be used exclusively for trap placing and AI movement

--- src/objects/misc/Grid.java
+++ src/objects/misc/Grid.java
@@ -1,17 +1,42 @@
 package objects.misc;
 
+import java.awt.image.BufferedImage;
+
 public class Grid {
-    private double[][] matrix = {
-            {1,1,1,0,1,1,1},
-            {1,0,0,0,0,0,0},
-            {1,0,0,0,0,0,1},
-            {1,0,0,0,0,0,1},
-            {1,0,0,0,0,0,1},
-            {1,0,0,0,0,0,1},
-            {1,1,1,1,1,1,1}
 
-    };
+    private int[][] matrix = new int[19][19];
     public Grid(){
+        BufferedImageLoader loader = new BufferedImageLoader();
+        readGridFromFile(loader.loadImage("/map/Floor1.png"));
+        for(int[] row : matrix) {
+            System.out.println();
+            for(int number : row) {
+                System.out.print(number+",");
+            }
+        }
+    }
 
+    private void readGridFromFile(BufferedImage image) {
+        int w = image.getWidth(); //gets the width of the image
+        int h = image.getHeight(); //gets the height of the image
+
+        for(int y = 0; y < h; y++) { //first for loop, starts in top right, progressively goes to the down
+            for(int x = 0; x < w; x++) { //second for loop, starts in top right, progressively moves right.
+                int pixel = image.getRGB(x, y);
+                int red = (pixel >> 16) & 0xff;
+                int green = (pixel >> 8) & 0xff;
+                int blue = (pixel) & 0xff;
+
+                if(red == 255 && green == 0 && blue == 0) {
+                    matrix[x][y] = 0;
+                    System.out.print("0,");
+                }
+                if(red == 0 && green == 255 && blue == 0) {
+                    matrix[x][y] = 1;
+                    System.out.print("1,");
+                }
+            }
+            System.out.println();
+        }
     }
 }
