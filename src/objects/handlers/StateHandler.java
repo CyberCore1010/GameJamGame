4d72486018c04604f804f90e93516ce48065bcf6
CyberCore1010 <39372030+CyberCore1010@users.noreply.github.com>
2019-01-26 21:16:48 +0000

    -More rooms

--- src/objects/handlers/StateHandler.java
+++ src/objects/handlers/StateHandler.java
@@ -25,13 +25,14 @@ public class StateHandler {
         BufferedImageLoader loader = new BufferedImageLoader();
         //Floor
         game.objectHandler.add(new Floor(300, 400, 200, 150, loader.loadImage("/floor/redBrick.png"), game));//Utility Room
-        game.objectHandler.add(new Floor(300, 550, 201, 450, loader.loadImage("/floor/wood.png"), game));//Garage
-        game.objectHandler.add(new Floor(501, 700, 299, 300, loader.loadImage("/floor/wood.png"), game));//Lounge
+        game.objectHandler.add(new Floor(300, 550, 201, 450, loader.loadImage("/floor/garage.png"), game));//Garage
         game.objectHandler.add(new Floor(501, 299, 400, 301, loader.loadImage("/floor/kitchen.png"), game));//Kitchen
         game.objectHandler.add(new Floor(900, 400, 100, 201, loader.loadImage("/floor/wood.png"), game));//Stairs
-        game.objectHandler.add(new Floor(1000, 400, 200, 201, loader.loadImage("/floor/wood.png"), game));//Study1
-        game.objectHandler.add(new Floor(1200, 400, 150, 201, loader.loadImage("/floor/wood.png"), game));//Study2
+        game.objectHandler.add(new Floor(1000, 400, 200, 201, loader.loadImage("/floor/carpet.png"), game));//Study1
+        game.objectHandler.add(new Floor(1200, 400, 150, 201, loader.loadImage("/floor/carpet.png"), game));//Study2
         game.objectHandler.add(new Floor(501, 600, 700, 101, loader.loadImage("/floor/wood.png"), game));//HallwayH1
+        game.objectHandler.add(new Floor(501, 700, 299, 300, loader.loadImage("/floor/wood.png"), game));//Lounge
+        game.objectHandler.add(new Floor(900, 700, 299, 200, loader.loadImage("/floor/carpet.png"), game));//Fireplace room
         game.objectHandler.add(new Floor(1201, 601, 100, 550, loader.loadImage("/floor/wood.png"), game));//HallwayH3
 
         PathList pathList = new PathList(new Node(402,649, game),game);
@@ -41,11 +42,12 @@ public class StateHandler {
         //Room Bounds
         game.objectHandler.add(new RoomBounds(300, 400, 200, 150, game));//Utility Room
         game.objectHandler.add(new RoomBounds(300, 550, 201, 450, game));//Garage
-        game.objectHandler.add(new RoomBounds(501, 700, 299, 300, game));//Lounge
         game.objectHandler.add(new RoomBounds(501, 299, 400, 301, game));//Kitchen
         game.objectHandler.add(new RoomBounds(1000, 400, 200, 200, game));//Study1
         game.objectHandler.add(new RoomBounds(1200, 400, 150, 201, game));//Study2
         game.objectHandler.add(new RoomBounds(501, 600, 700, 101, game));//HallwayH1
+        game.objectHandler.add(new RoomBounds(501, 700, 299, 300, game));//Lounge
+        game.objectHandler.add(new RoomBounds(900, 700, 299, 200, game));//Fireplace room
         game.objectHandler.add(new RoomBounds(1201, 601, 100, 550, game));//HallwayH3
 
         //Player
@@ -65,11 +67,6 @@ public class StateHandler {
         game.objectHandler.add(new Wall(497, 295, 409, 10, game));
         game.objectHandler.add(new Wall(896, 300, 10, 305, game));
         game.objectHandler.add(new Wall(875, 595, 30, 10, game));
-
-        //Lounge
-        game.objectHandler.add(new Wall(496, 995, 309, 10, game));
-        game.objectHandler.add(new Wall(795, 700, 10, 25, game));
-        game.objectHandler.add(new Wall(795, 775, 10, 225, game));
 
         //Stairs
         game.objectHandler.add(new Wall(900, 396, 100, 10, game));
@@ -91,6 +88,17 @@ public class StateHandler {
         game.objectHandler.add(new Wall(497, 675, 10, 50, game));
         game.objectHandler.add(new Wall(497, 696, 328, 10, game));
         game.objectHandler.add(new Wall(875, 696, 328, 10, game));
+
+        //Lounge
+        game.objectHandler.add(new Wall(496, 995, 309, 10, game));
+        game.objectHandler.add(new Wall(795, 700, 10, 25, game));
+        game.objectHandler.add(new Wall(795, 775, 10, 225, game));
+
+        //Fireplace room/Dining room
+        game.objectHandler.add(new Wall(895, 700, 10, 25, game));
+        game.objectHandler.add(new Wall(895, 895, 300, 10, game));
+        game.objectHandler.add(new Wall(895, 775, 10, 375, game));
+        game.objectHandler.add(new Wall(895, 1145, 330, 10, game));
 
         //HallwayH3
         game.objectHandler.add(new Wall(1295, 600, 10, 650, game));
